06b507f0b36009c92b06ef1862e5afd2c8fd688d
westerndundrey <hijackedpuffin@gmail.com>
2024-05-26 16:47:22 +0800

    variables

--- hangman.py
+++ hangman.py
@@ -1,4 +1,9 @@
 import pygame
 pygame.init()
-window = pygame.display.set_mode((width, height))
+window = pygame.display.set_mode(width=800, height=600)
 pygame.display.set_caption("Hangman")
+
+white = (255,255,255)
+black = (0,0,0)
+font_size = 40
+
