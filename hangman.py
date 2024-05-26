4d9d84922532ed020478eafb1ef8512447fce7d7
westerndundrey <hijackedpuffin@gmail.com>
2024-05-26 16:52:35 +0800

    def draw

--- hangman.py
+++ hangman.py
@@ -7,3 +7,19 @@ white = (255,255,255)
 black = (0,0,0)
 font_size = 40
 
+hangman_images = [pygame.image.load(f"hangman{i}.png") for i in range(7)]
+font = pygame.front.Sysfont('arial', font_size)
+
+def draw():
+    window.fill(white)
+    display_word = ""
+    for letter in word:
+        if letter in guessed:
+            display_word += letter + ""
+        else:
+            display_word +="_ "
+    text = font.render(display_word,True, black)
+    window.blit(text,(400,200))
+    window.blit(hangman_images,[hangman_status], (150,100))
+    pygame.display.update()
+
